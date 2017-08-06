3d5ce3176e99140f8a3a2daabe36a62fbb47f250
arahansa <arahansa@naver.com>
2017-08-07 01:46:08 +0900

    2주차 : 바인딩 기초 :: 단방향, 양방향 까지 처리

--- references/refer2/src/app/app.component.ts
+++ references/refer2/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // 삽입식 바인딩
   title = 'app works!';
+
+  // 프로퍼티 바인딩
+  myStyle = {color:'blue', 'font-weight':800, 'font-size' : '0.7rem'};
+
+  // 이벤트 바인딩
+  evtBinding(): void {
+    alert("헬로 월드?");
+  }
+
+  // 양방향 바인딩
+  way2 = "way2..";
+
+
+
 }
